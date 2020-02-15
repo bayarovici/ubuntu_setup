c4f523979dbda185c25fdfa4ab2fa7bd9b3f8d7d
bayarovici <bayar.menzat@cncb.ox.ac.uk>
2020-02-15 11:00:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tmux-resurrect scripts/config.sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
