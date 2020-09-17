9922e84de724b95babfff2db4bb0255e5c518256
bayarovici <bayar.menzat@akriviahealth.com>
2020-09-17 09:13:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+sudo apt install curl
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tmux-resurrect scripts/config.sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
